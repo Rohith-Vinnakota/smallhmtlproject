61c79e88a830f59e2c0046b0bfaa29e16c1c6570
Vinnakotarohith <90550145+rohithvinnakota@users.noreply.github.com>
2023-08-31 22:46:25 +0530

    commit by rohith

--- TextDecoration/index.js
+++ TextDecoration/index.js
@@ -1,28 +1,20 @@
 var text = document.getElementById('input_text');
 var result = document.getElementById('result');
-function display() {
-
+ display=() =>{
     result.value = text.value;
 }
-console.log(result.value);
-function Lower() {
-    console.log(result.value);
+ Lower=()=> {
+    result.style.textDecoration = 'underline';
     result.value = result.value.toLowerCase();
-
 }
-function Upper() {
-    console.log(result.value);
-
+ Upper=()=> {
+    result.style.textDecoration = 'underline';
     result.value = result.value.toUpperCase();
-
 }
-function underLine() {
-
+ underLine=()=> {
     result.style.textDecoration = 'underline';
 }
-function lineThrough() {
-    console.log(result.value);
-
+ lineThrough=()=> {
     result.style.textDecoration = 'line-through';
 
 }
